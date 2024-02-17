b7e8e0d6a7ac90efa8fa47211e8aff498e77c161
Phillip Clontz <PhillipAC718@gmail.com>
2024-02-17 15:25:03 -0600

    Fixed the typo in the rest of the project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Destionations
+# Destinations
 
-Destionations is an application used for creating, sharing, and playing live action games where the objective is to move between destinations. 
+Destinations is an application used for creating, sharing, and playing live action games where the objective is to move between destinations. 
 
 Games such as:
 Hide & Seek or Tag where the hiders/runners have objectives to get to in order to win.
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Destionations' title`, () => {
+  it(`should have the 'Destinations' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Destionations');
+    expect(app.title).toEqual('Destinations');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Destionations');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Destinations');
   });
 });
