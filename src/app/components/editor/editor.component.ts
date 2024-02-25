2a76178990cdbbc2ccf46ee21117e5fde0f93a26
Phillip Clontz <phillipac718@gmail.com>
2024-02-25 17:44:58 -0600

    Cleaned up code by moving logic to editor service

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -19,12 +19,12 @@ import { ArrayHelper } from '../../helpers/array-helper';
   styleUrl: './editor.component.scss'
 })
 export class EditorComponent implements OnInit{
+
   public gameName: string = "New Game";
   public gameTag: string = "Tag for the game";
-  public areas: AreaEditor[] = [
-    new AreaEditor(0, "Upstairs", [new LocationEditor(0, "Bedroom"), new LocationEditor(1, "Bathroom")], [1]),
-    new AreaEditor(1, "Ground Floor", [new LocationEditor(2, "Front Bedroom"), new LocationEditor(3, "Front Bathroom"), new LocationEditor(4, "Kitchen")], [])
-  ];
+  public get areas(): AreaEditor[]{
+    return this._editorService.areas
+  }
 
   public get arrayHelper(): typeof ArrayHelper {
     return ArrayHelper;
@@ -33,58 +33,30 @@ export class EditorComponent implements OnInit{
   constructor(private _editorService: EditorService){}
 
   public ngOnInit(): void {
-    this._editorService.loadConfigFromService().subscribe(_ => {
-      console.log(this._editorService.areas);
-      this.areas = this._editorService.areas;
-    });
-  }
-
-  public newArea(): void{
-    this.areas.push(new AreaEditor(ArrayHelper.nextId(this.areas), "New Area", [], []));
-  }
-
-  public newLocation(areaId: number){
-    let area = ArrayHelper.selectById(this.areas, areaId);
-    if(area != null){
-      area.locations.push(new LocationEditor(ArrayHelper.nextId(area.locations), "New Location"));
-    }
-  }
-
-  public removeLocation(locationId: number){
-    this.areas.forEach(a => {
-      ArrayHelper.removeById(a.locations, locationId);
-    })
-  }
-
-  public removeArea(areaId: number){
-    this.areas.forEach(a => {
-      ArrayHelper.removeFromArray(a.adjacentAreas, areaId);
-    })
-    ArrayHelper.removeById(this.areas, areaId);
-  }
-
-  public addAdjacentArea(areaId: number, adjacentAreaId: number): void{
-    let area = ArrayHelper.selectById(this.areas, areaId);
-    let exists = area?.adjacentAreas.find(a => a == adjacentAreaId) != undefined;
-    if(area != null && !exists){
-      area.adjacentAreas.push(adjacentAreaId);
-    }
-  }
-
-  public removeAdjacentArea(areaId: number, adjacentAreaId: number): boolean{
-    let area = ArrayHelper.selectById(this.areas, areaId);
-    if(area != null){
-      return ArrayHelper.removeFromArray(area.adjacentAreas, adjacentAreaId);
-    }
-    return false;
+    this._editorService.loadConfigFromService().subscribe();
   }
 
   public handleAdjacentAreaToggle(event: Event, areaId: number, adjacentAreaId: number): void{
-    let removed = this.removeAdjacentArea(areaId, adjacentAreaId);
-    console.log(removed);
+    let removed = this._editorService.removeAdjacentArea(areaId, adjacentAreaId);
     if(!removed){
-      this.addAdjacentArea(areaId, adjacentAreaId);
+      this._editorService.addAdjacentArea(areaId, adjacentAreaId);
     }
+  }
+
+  public newLocation(areaId: number) {
+    this._editorService.newLocation(areaId);
+  }
+
+  public removeLocation(locationId: number) {
+    this._editorService.removeLocation(locationId);
+  }
+
+  public removeArea(areaId: number) {
+    this._editorService.removeArea(areaId);
+  }
+
+  public newArea() {
+    this._editorService.newArea();
   }
 
   public post(){
--- src/app/services/editor.service.ts
+++ src/app/services/editor.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { GameConfigService } from './game-config.service';
 import { AreaEditor } from '../models/editor/area-editor';
 import { LocationEditor } from '../models/editor/location-editor';
-import { Observable, flatMap, map, mergeMap, of, switchMap, tap, withLatestFrom } from 'rxjs';
-import { TmplAstInteractionDeferredTrigger } from '@angular/compiler';
+import { Observable, map, of } from 'rxjs';
+import { ArrayHelper } from '../helpers/array-helper';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +27,46 @@ export class EditorService {
         this.loadEditorService();
         return this.areas;
       }));
+  }
+
+  public newArea(): void{
+    this.areas.push(new AreaEditor(ArrayHelper.nextId(this.areas), "New Area", [], []));
+  }
+
+  public newLocation(areaId: number){
+    let area = ArrayHelper.selectById(this.areas, areaId);
+    if(area != null){
+      area.locations.push(new LocationEditor(ArrayHelper.nextId(area.locations), "New Location"));
+    }
+  }
+
+  public removeLocation(locationId: number){
+    this.areas.forEach(a => {
+      ArrayHelper.removeById(a.locations, locationId);
+    })
+  }
+
+  public removeArea(areaId: number){
+    this.areas.forEach(a => {
+      ArrayHelper.removeFromArray(a.adjacentAreas, areaId);
+    })
+    ArrayHelper.removeById(this.areas, areaId);
+  }
+
+  public addAdjacentArea(areaId: number, adjacentAreaId: number): void{
+    let area = ArrayHelper.selectById(this.areas, areaId);
+    let exists = area?.adjacentAreas.find(a => a == adjacentAreaId) != undefined;
+    if(area != null && !exists){
+      area.adjacentAreas.push(adjacentAreaId);
+    }
+  }
+
+  public removeAdjacentArea(areaId: number, adjacentAreaId: number): boolean{
+    let area = ArrayHelper.selectById(this.areas, areaId);
+    if(area != null){
+      return ArrayHelper.removeFromArray(area.adjacentAreas, adjacentAreaId);
+    }
+    return false;
   }
 
   private loadEditorService(): void{
