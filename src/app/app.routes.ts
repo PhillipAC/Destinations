05018ca18f08ce5ce712e329e1c9fa6ed806194e
Phillip Clontz <phillipac718@gmail.com>
2024-03-03 09:16:56 -0600

    Added an about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { EditorComponent } from './components/editor/editor.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { GameComponent } from './components/game/game.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomeComponent },
     { path: 'editor', component: EditorComponent},
-    { path: 'game', component: GameComponent}
+    { path: 'game', component: GameComponent},
+    { path: 'about', component: AboutComponent}
 ];
